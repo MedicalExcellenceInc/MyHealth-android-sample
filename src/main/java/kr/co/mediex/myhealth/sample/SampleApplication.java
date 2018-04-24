ec0ac7d0c9f8c635978293a20f4e85e412e118ab
Park9eon <quiet9.noise@gmail.com>
2018-04-24 11:30:57 +0900

    어플리케이션 파일 추가

---
+++ src/main/java/kr/co/mediex/myhealth/sample/SampleApplication.java
@@ -0,0 +1,15 @@
+package kr.co.mediex.myhealth.sample;
+
+import android.app.Application;
+
+// 샘플 어플리케이션
+public class SampleApplication extends Application {
+
+    // 서비스 이름
+    public static String MY_SERVICE_NAME = "";
+    // 서비스 생성 계정 비밀번호
+    public static String MY_SERVICE_SECRET = "";
+    // 리소스 이름
+    public static String MY_RESOURCE_NAME = "";
+
+}
