f64b3c1ed9fe8b9d0f9b0a0e4da76d51b8388a2c
roundmid00 <jb.song@mediex.co.kr>
2018-04-24 15:56:27 +0900

    CRUD 예제 추가

--- src/main/java/kr/co/mediex/myhealth/sample/views/SampleSaveDataActivity.java
+++ src/main/java/kr/co/mediex/myhealth/sample/views/SampleSaveDataActivity.java
@@ -1,13 +1,16 @@
 package kr.co.mediex.myhealth.sample.views;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import kr.co.mediex.myhealth.sample.R;
 import kr.co.mediex.myhealth.sample.SampleApplication;
 import kr.co.mediex.myhealth.sample.models.MyBodyInfo;
@@ -20,6 +23,8 @@ import kr.co.mediex.myhealth.v1.function.Success;
 import retrofit2.HttpException;
 
 import java.net.SocketTimeoutException;
+import java.util.List;
+import java.util.Map;
 
 // 로그인 후 유저정보를 확인하거나 샘플 데이터를 보내볼 수 있음.
 public class SampleSaveDataActivity extends AppCompatActivity implements View.OnClickListener {
@@ -47,7 +52,11 @@ public class SampleSaveDataActivity extends AppCompatActivity implements View.On
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.saveButton:
-                this.saveData();
+                this.saveData();    // 저장 관련 예제
+//                this.updateData();    // 수정 관련 예제
+//                this.deleteData();    // 삭제 관련 예제
+//                this.findData();      // 상세 조회 관련 예제
+//                this.findAllData();   // 페이지별 조회 관련 예제
                 break;
             case R.id.logoutButton:
                 this.logout();
@@ -116,6 +125,42 @@ public class SampleSaveDataActivity extends AppCompatActivity implements View.On
         });
     }
 
+    private void alert(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SampleSaveDataActivity.this);
+                builder.setCancelable(true)
+                        .setMessage(message)
+                        .setNegativeButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+
+    // myHealth 연동 관련 에러 핸들러
+    private Error errorHandler = new Error() {
+        @Override
+        public void onError(Throwable throwable) {
+            throwable.printStackTrace();
+            if (throwable instanceof SocketTimeoutException) {
+                showToast("인터넷연결상태를 확인해주세요.");
+            } else if (throwable instanceof HttpException) {
+                showToast("서버 에러");
+            } else {
+                throwable.printStackTrace();
+                showToast("에러");
+            }
+        }
+    };
+
     // 샘플 데이터를 저장해 볼 수 있습니다.
     private void saveData() {
         try {
@@ -137,28 +182,88 @@ public class SampleSaveDataActivity extends AppCompatActivity implements View.On
             myBodyInfo.setMyUser(myUser);
 
             mMyHealthService.insertResource(SampleApplication.MY_RESOURCE_NAME,
+                myBodyInfo,
+                    new Success<Object>() {
+                        // 샘플 정보를 서버에서 저장하기 정공
+                        @Override
+                        public void onSuccess(Object o) {
+                            showToast("성공적으로 데이터가 저장되었습니다.");
+                        }
+                    }, errorHandler);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateData() {
+        try {
+            // TODO 데이터 유효성검사 필요!
+            Double weight = Double.valueOf(this.mWeightEditText.getText().toString());
+            Double height = Double.valueOf(this.mHeightEditText.getText().toString());
+
+            // 서비스 사용자 정보
+            MyUser myUser = new MyUser();
+            // 샘플 유저 아이디를 지정
+            myUser.setId((long) (Math.random() * 1000));
+            myUser.setName("myUserName");
+
+            MyBodyInfo myBodyInfo = new MyBodyInfo();
+            // 샘플 신체정보 아이디 지정
+            myBodyInfo.setId((long) (Math.random() * 1000));
+            myBodyInfo.setWeight(weight);
+            myBodyInfo.setHeight(height);
+            myBodyInfo.setMyUser(myUser);
+
+            mMyHealthService.updateResource(SampleApplication.MY_RESOURCE_NAME, "3",
                     myBodyInfo,
                     new Success<Object>() {
                         // 샘플 정보를 서버에서 저장하기 정공
                         @Override
                         public void onSuccess(Object o) {
-                            showToast("성공적으로 데이터를 전송하였습니다.");
+                            showToast("성공적으로 데이터가 수정되었습니다.");
                         }
-                    }, new Error() {
-                        // 샘플 정보를 서버에서 저장하기 실패
+                    }, errorHandler);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteData() {
+        try {
+            mMyHealthService.deleteResource(SampleApplication.MY_RESOURCE_NAME, "2", new Success<Object>() {
+                @Override
+                public void onSuccess(Object o) {
+                    showToast("성공적으로 데이터를 삭제하였습니다.");
+                }
+            }, errorHandler);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void findData() {
+        try {
+            mMyHealthService.findResource(SampleApplication.MY_RESOURCE_NAME, "4",
+                    new Success<Map<String, Object>>() {
                         @Override
-                        public void onError(Throwable throwable) {
-                            throwable.printStackTrace();
-                            if (throwable instanceof SocketTimeoutException) {
-                                showToast("인터넷연결상태를 확인해주세요.");
-                            } else if (throwable instanceof HttpException) {
-                                showToast("서버 에러");
-                            } else {
-                                throwable.printStackTrace();
-                                showToast("에러");
-                            }
+                        public void onSuccess(Map<String, Object> stringObjectMap) {
+                            alert(stringObjectMap.toString());
                         }
-                    });
+                    }, errorHandler);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void findAllData() {
+        try {
+            mMyHealthService.findResources(SampleApplication.MY_RESOURCE_NAME, "1",
+                    new Success<List<Map<String, Object>>>() {
+                        @Override
+                        public void onSuccess(List<Map<String, Object>> maps) {
+                            alert(maps.toString());
+                        }
+                    }, errorHandler);
         } catch (Throwable e) {
             e.printStackTrace();
         }
